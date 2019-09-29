70b5c793c81bfec03a44c5e2cd54845b089c1600
Yaromik Evgeni <dom2kb1@yandex.ru>
2019-09-29 17:05:28 +0300

    beta 3

--- force-app/main/default/lwc/weatherView/weatherView.js
+++ force-app/main/default/lwc/weatherView/weatherView.js
@@ -1,31 +1,14 @@
-import { LightningElement, track, wire} from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getWeather from '@salesforce/apex/WeatherViewController.getWeather';
-
-// const DELAY = 300;
 
 export default class WeatherView extends LightningElement {
 
     searchKey = '';
-    @track weather;
+    @track weather; //use parse to JSON  
     @track imgUrl;
     @track error;
 
-    // @wire(getWeather, { searchKey: '$searchKey' })
-    // wiredWeather({ error, data }) {
-    //     if (data) {
-    //         this.weather = data;
-    //         this.error = undefined;
-    //         this.imgUrl = '';
-    //         this.imgUrl = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-
-    //         // eslint-disable-next-line no-console
-    //         console.log('weather-url ' + this.imgUrl);
-    //     } else if (error) {
-    //         this.error = error;
-    //         this.weather = undefined;
-    //     }
-    // }
-
+    // load weather on open page
     @wire(getWeather, { searchKey: '$searchKey' })
     wiredWeather({ error, data }) {
         if (data) {
@@ -36,59 +19,16 @@ export default class WeatherView extends LightningElement {
             this.weather.main.temp = Math.floor(this.weather.main.temp);
             this.weather.main.temp_min = Math.floor(this.weather.main.temp_min);
             this.weather.main.temp_max = Math.floor(this.weather.main.temp_max);
-
-            // eslint-disable-next-line no-console
-            console.log('weather-dd ' + this.weather);
         } else if (error) {
-            // eslint-disable-next-line no-console
-            console.log('error= ' + error.body.message);
             this.error = JSON.parse(error.body.message);
             this.weather = undefined;
         }
     }
 
-    setSearchKey(){
+    // set search city in input text
+    setSearchKey() {
         var inp = this.template.querySelector('.slds-input').value
-        this.searchKey=inp;
-        // eslint-disable-next-line no-console
-        console.log(inp);
+        this.searchKey = inp;
     }
 
-    handleKeyChange(event) {
-        // Debouncing this method: Do not update the reactive property as long as this function is
-        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
-        // window.clearTimeout(this.delayTimeout);
-        this.searchKey = event.target.value;
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        // this.delayTimeout = setTimeout(() => {
-        //     this.searchKey = searchKey;
-        // }, DELAY);
-    }
-    // render(){
-    //     this.render = false;
-    // }
-
-
-    // renderedCallback() {
-    //     if (this.render) {
-    //         // eslint-disable-next-line no-console
-    //     console.log('if render render');
-    //         return;
-    //     }
-    //     getWeather({ searchKey: this.searchKey})
-    //         .then(result => {
-    //             this.weather = result;
-    //             this.error = undefined;
-    //             this.imgUrl = '';
-    //             this.imgUrl = 'http://openweathermap.org/img/wn/' + result.weather[0].icon + '@2x.png';
-
-    //             // eslint-disable-next-line no-console
-    //             console.log('weather-ddd ' + this.weather.base);
-    //         })
-    //         .catch(error => {
-    //             this.error = error;
-    //             this.weather = undefined;
-    //         });
-    //     this.render = true;
-    // }
 }
